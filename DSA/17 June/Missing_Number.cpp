a599a7385afe9cc354fd96e9ed9080d8a23704aa
Lael-Mariya <laelmariyab@gmail.com>
2025-06-18 23:19:10 +0530

    Update Missing_Number.cpp

--- DSA/17 June/Missing_Number.cpp
+++ DSA/17 June/Missing_Number.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int len= nums.size();
+        int xr1=0, xr2=0;
+        for(int i=0; i<len; i++){
+            xr1=xr1^nums[i];
+            xr2=xr2^i;
+        }
+        xr2=xr2^len;
+        return xr1^xr2;        
+    }
+};
