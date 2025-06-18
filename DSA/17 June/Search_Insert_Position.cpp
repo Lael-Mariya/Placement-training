e95415750f7cc487787b38e82287994a46f99aca
Lael-Mariya <laelmariyab@gmail.com>
2025-06-18 23:28:16 +0530

    Update Search_Insert_Position.cpp

--- DSA/17 June/Search_Insert_Position.cpp
+++ DSA/17 June/Search_Insert_Position.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int len=nums.size();
+        int low=0, high=len-1;
+        int ans=len;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            if (nums[mid]>=target){ //eliminating the right part of the array as this is a soreted array
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+            low=mid+1;
+        }
+    }
+        return ans;
+    }
+};
