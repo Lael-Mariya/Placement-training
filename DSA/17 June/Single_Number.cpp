79157610bdd9cb576882156fa25ab914f7458cb7
Lael-Mariya <laelmariyab@gmail.com>
2025-06-18 23:26:10 +0530

    Update Single_Number.cpp

--- DSA/17 June/Single_Number.cpp
+++ DSA/17 June/Single_Number.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int len= nums.size();
+        int xr=0;
+        for(int i=0;i<len; i++ ){
+            xr=xr^nums[i];
+        }
+        return xr;
+    }
+};
