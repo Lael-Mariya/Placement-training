ee1e7b0e925bbac0d38e77501235b4da4d3f90ce
Lael-Mariya <laelmariyab@gmail.com>
2025-06-18 23:23:01 +0530

    Update Reverse_Digit.cpp

--- DSA/17 June/Reverse_Digit.cpp
+++ DSA/17 June/Reverse_Digit.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int reverseDigits(int n) {
+        int rev =0;
+        int rem;
+        while (n>0) {
+            rem=n%10;
+            rev =rev* 10 + rem;
+            n = n/10;
+        }
+        return rev;
+    }
+};
