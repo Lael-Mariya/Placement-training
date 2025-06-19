f914ff0e732139c82b35ca22e7dfaa7322282327
Lael-Mariya <laelmariyab@gmail.com>
2025-06-19 19:42:17 +0530

    Update Two_Sum.cpp

--- DSA/June 18/Two_Sum.cpp
+++ DSA/June 18/Two_Sum.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len=nums.size();
+        int low=0, high=len-1;
+        vector<pair<int, int>> arr;    
+        for (int i=0; i<len; i++) {
+            arr.push_back({nums[i],i});
+        }  
+        sort(arr.begin(), arr.end());     
+        while (low<high){
+            int sum=arr[low].first + arr[high].first;         
+            if (sum>target){
+                high--;
+            }
+            else if (sum<target){
+                low++;
+            }
+            else{
+                return {arr[low].second,arr[high].second}; 
+            }
+        }     
+        return {};
+    }
+};
 
