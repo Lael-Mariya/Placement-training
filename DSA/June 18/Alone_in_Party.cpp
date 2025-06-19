d83d89e458de432d811ac050dc43560698b07206
Lael-Mariya <laelmariyab@gmail.com>
2025-06-19 19:44:27 +0530

    Update Alone_in_Party.cpp

--- DSA/June 18/Alone_in_Party.cpp
+++ DSA/June 18/Alone_in_Party.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+  public:
+    int findSingle(vector<int> &arr) {
+        int len= arr.size();
+        int xr=0;
+        for(int i=0; i<len; i++){
+            xr=xr^arr[i];
+        }
+        return xr;
+    }
+};
