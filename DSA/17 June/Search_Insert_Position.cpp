afce283dafe932b4355c5950a552d0947475cde9
Lael-Mariya <laelmariyab@gmail.com>
2025-06-24 21:17:54 +0530

    Update Search_Insert_Position.cpp

--- DSA/17 June/Search_Insert_Position.cpp
+++ DSA/17 June/Search_Insert_Position.cpp
@@ -1,3 +1,5 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// Time Complexity: O(log n)
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
