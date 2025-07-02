88f578ba77064e1b9a8071d3aa76c68c5699e1af
Lael-Mariya <laelmariyab@gmail.com>
2025-07-02 19:33:38 +0530

    Update Two_Sum.cpp

--- DSA/18 June/Two_Sum.cpp
+++ DSA/18 June/Two_Sum.cpp
@@ -1,5 +1,5 @@
 // Given an array of integers arr[] and an integer target. Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
-// 
+// Time Complexity: O(nlogn)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -7,7 +7,7 @@ public:
         int low=0, high=len-1;
         vector<pair<int, int>> arr;    // {value, og index}
         for (int i=0; i<len; i++) {
-            arr.push_back({nums[i],i});   // store the elements in pairs with their og indices, to retrieve the original indices of the unsorted array as the array is sorted for further operation. 
+            arr.push_back({nums[i],i});   // stores the elements in pairs with their og indices, to retrieve the og indices of the unsorted array as the array is sorted for further operation. 
         }  
         sort(arr.begin(), arr.end());     
         while (low<high){
