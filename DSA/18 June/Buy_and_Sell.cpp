4b874cfd0d2805e28f4bd167249794c6bf093071
Lael-Mariya <laelmariyab@gmail.com>
2025-07-02 20:14:39 +0530

    Update Buy_and_Sell.cpp

--- DSA/18 June/Buy_and_Sell.cpp
+++ DSA/18 June/Buy_and_Sell.cpp
@@ -1,7 +1,7 @@
 // You are given an array of prices where prices[i] is the price of a given stock on an ith day.
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. 
 // If you cannot achieve any profit, return 0.
-// O(n) Time Complexity
+// Time Complexity: O(n)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
