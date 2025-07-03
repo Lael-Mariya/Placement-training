719938af301836c4046511affa18daa6adb1f783
Lael-Mariya <laelmariyab@gmail.com>
2025-07-03 21:05:58 +0530

    Update Plus_One.cpp

--- DSA/19 June/Plus_One.cpp
+++ DSA/19 June/Plus_One.cpp
@@ -1,15 +1,18 @@
- #include <vector>
+// Time Complexity: O(n)
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for (int i=digits.size()-1; i>=0; --i){
-            digits[i]++;
-            digits[i]%=10;
-            if (digits[i]!=0){
+        int n=digits.size();
+        for (int i=n-1; i>=0; --i) {
+            if(digits[i]==9){
+                digits[i]=0; // doesn't return condition
+            }
+            else{
+                digits[i]++;
                 return digits;
             }
         }
-        digits.insert(digits.begin(), 1);
+        digits.insert(digits.begin(),1);   // if the for loop didn't return digits, all the numbers were 9, as the if condition doesn't return digits
         return digits;
     }
 };
