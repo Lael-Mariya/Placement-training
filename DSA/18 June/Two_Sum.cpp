a730ff3072a0d2ad3bbb1c96d223241eafcf01d4
Lael-Mariya <laelmariyab@gmail.com>
2025-07-03 17:29:47 +0530

    Update Two_Sum.cpp

--- DSA/18 June/Two_Sum.cpp
+++ DSA/18 June/Two_Sum.cpp
@@ -1,4 +1,4 @@
-// Given an array of integers arr[] and an integer target. Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+// Given an array of integers arr[] and an integer target. Return indices of the two numbers such that their sum is equal to the target.
 // Time Complexity: O(n logn)
 class Solution {
 public:
@@ -25,4 +25,20 @@ public:
         return {};
     }
 };
-
+// Using Hash table
+// Time Complexity: O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for (int i=0; i<nums.size(); i++){
+            int complement= target-nums[i];  
+            if (mpp.find(complement)!=mpp.end()){ // find for the complement, if present then return indices
+                return {mpp[complement],i};
+            }
+            mpp[nums[i]]=i; // not found so store the num (key) with value (index), because we check if the complement is there not in the map later
+        }
+        return{};
+       
+    }
+};
