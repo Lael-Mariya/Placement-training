4b1a4c6eaffb126c7ec72a520853ea825b05a12b
Lael-Mariya <laelmariyab@gmail.com>
2025-07-10 15:32:03 +0300

    Update 3Sum.cpp

--- DSA/20 June/3Sum.cpp
+++ DSA/20 June/3Sum.cpp
@@ -1,3 +1,4 @@
+// YUsing two-pointer approach, Time Complexity=O(nlogn + n^2)
 #include <vector>
 #include <algorithm>
 class Solution {
@@ -7,7 +8,7 @@ public:
         vector<vector<int>> result;
         int n = nums.size();      
         for (int i = 0; i < n - 2 && nums[i] <= 0; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1])  
+            if (i > 0 && nums[i] == nums[i - 1])  // optimisation for checking repeated values
                 continue;        
             int j = i + 1, k = n - 1;
             while (j < k) {
@@ -18,8 +19,8 @@ public:
                     result.push_back({nums[i], nums[j], nums[k]});
                     ++j;
                     --k;
-                    while (j < k && nums[j] == nums[j - 1]) ++j;
-                    while (j < k && nums[k] == nums[k + 1]) --k;
+                    while (j < k && nums[j] == nums[j - 1]) ++j; // optimisation for checking repeated values
+                    while (j < k && nums[k] == nums[k + 1]) --k; // optimisation for checking repeated values
                 }
             }
         }       
