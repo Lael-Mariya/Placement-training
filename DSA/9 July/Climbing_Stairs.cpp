e567122c4fb37cf7dc4afc55d71c9261c949d2cb
Lael-Mariya <laelmariyab@gmail.com>
2025-07-12 17:57:30 +0300

    Update Climbing_Stairs.cpp

--- DSA/9 July/Climbing_Stairs.cpp
+++ DSA/9 July/Climbing_Stairs.cpp
@@ -11,3 +11,13 @@ public:
         return curr;
     }
 };
+// recursive
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n==1) return 1;
+        if (n==2) return 2;
+        
+        return climbStairs(n-2) + climbStairs(n-1);
+    }
+};
