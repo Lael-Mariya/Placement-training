45f501c4c1e7f089cd68404ea9d5a78fe0172dce
Lael-Mariya <laelmariyab@gmail.com>
2025-07-30 17:46:29 +0530

    Create Max_Twin_Sum.cpp

---
+++ DSA/27 June/Max_Twin_Sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+ public:
+  int pairSum(ListNode* head) {
+    int ans = 0;
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    ListNode* tail = reverseList(slow);
+
+    while (tail != nullptr) {
+      ans = max(ans, head->val + tail->val);
+      head = head->next;
+      tail = tail->next;
+    }
+
+    return ans;
+  }
+
+ private:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    while (head != nullptr) {
+      auto next = head->next;
+      head->next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
+  }
+};
