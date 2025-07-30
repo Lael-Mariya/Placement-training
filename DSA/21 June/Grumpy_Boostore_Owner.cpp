300755f37abbb7282f26824544f56c3a600da7f8
Lael-Mariya <laelmariyab@gmail.com>
2025-07-30 17:35:51 +0530

    Update Grumpy_Boostore_Owner.cpp

--- DSA/21 June/Grumpy_Boostore_Owner.cpp
+++ DSA/21 June/Grumpy_Boostore_Owner.cpp
@@ -1,20 +1,20 @@
 class Solution {
-public:
-    int maxSatisfied(vector<int>& cust, vector<int>& grmpy, int minWin) {
-        int totalUnsat=0, total=0, windowUnsat=0, maxSat=0, n=cust.size();
-        for(int i=0;i<n;++i) {
-            totalUnsat+=cust[i]*grmpy[i];
-            total+=cust[i];
-        }
-        for(int i=0;i<n;++i) {
-            windowUnsat+=cust[i]*grmpy[i];
-            int start=i-minWin;
-            if(start>=0) {
-                maxSat=max(maxSat,total-(totalUnsat-windowUnsat));
-                windowUnsat-=cust[start]*grmpy[start];
-            }
-        }
-        maxSat=max(maxSat,total-(totalUnsat-windowUnsat));
-        return maxSat;
+ public:
+  int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
+    int satisfied = 0;
+    int madeSatisfied = 0;
+    int windowSatisfied = 0;
+
+    for (int i = 0; i < customers.size(); ++i) {
+      if (grumpy[i] == 0)
+        satisfied += customers[i];
+      else
+        windowSatisfied += customers[i];
+      if (i >= X && grumpy[i - X] == 1)
+        windowSatisfied -= customers[i - X];
+      madeSatisfied = max(madeSatisfied, windowSatisfied);
     }
+
+    return satisfied + madeSatisfied;
+  }
 };
